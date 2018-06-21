1776f918eb36cbe0348a1487ff4367e6db64b684
Felix Guo <felixguoxiuping@gmail.com>
2018-06-20 22:27:43 -0700

    WIP

--- src/editor.h
+++ src/editor.h
@@ -17,6 +17,21 @@ class Editor: public Pane {
 	int current_line = 0;
 	int current_col = 0;
 	int window_start = 0;
+
+	// For when you move cursor past an empty line
+	int phantom_col_pos = 0;
+	void updateColWithPhantom() {
+		int end_col = buffer->getLineLength(current_line);
+		if (current_col < phantom_col_pos) {
+			current_col = phantom_col_pos;
+		}
+		if (current_col > end_col) {
+			current_col = end_col;
+		}
+		else {
+			phantom_col_pos = current_col;
+		}
+	}
 public:
 	Editor(Yate &yate, Buffer *buffer, int x, int y, int width, int height) :
 		Pane(x, y, width, height), yate(yate), buffer(buffer) {
@@ -29,24 +44,41 @@ public:
 	// Because C++ delete is keyword...
 	void _delete();
 	void left() {
-		if (current_col != 0) current_col--;
+		if (current_col != 0) {
+			current_col--;
+			phantom_col_pos = current_col;
+		}
 	}
 	void right() {
-		if (current_col != buffer->getLineLength(current_line)) current_col++;
+		if (current_col != buffer->getLineLength(current_line)) {
+			current_col++;
+			phantom_col_pos = current_col;
+		}
 	}
 	void up() {
-		if (current_line != 0) current_line--;
-		current_col = std::min(current_col, buffer->getLineLength(current_line));
+		if (current_line != 0) {
+			current_line--;
+			updateColWithPhantom();
+		}
 	}
 	void down() {
-		if (current_line != buffer->size() - 1) current_line++;
-		current_col = std::min(current_col, buffer->getLineLength(current_line));
+		if (current_line != buffer->size() - 1) {
+			current_line++;
+			updateColWithPhantom();
+		}
 	}
 	void home() {
-		current_col = 0;
+		if (current_col != 0) {
+			current_col = 0;
+			phantom_col_pos = current_col;
+		}
 	}
 	void end() {
-		current_col = buffer->getLineLength(current_line);
+		int end_col = buffer->getLineLength(current_line);
+		if (current_col != end_col) {
+			current_col = end_col;
+			phantom_col_pos = current_col;
+		}
 	}
 	void save();
 	void focus();
