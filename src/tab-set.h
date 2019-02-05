d372d89021a3bf1fffaa06f444244b24c4b45461
Felix Guo <felixguoxiuping@gmail.com>
2019-02-05 09:50:21 -0500

    WIP

--- src/tab-set.h
+++ src/tab-set.h
@@ -14,16 +14,22 @@
 class TabSet : public Pane {
   Yate &yate;
   std::vector<PaneSet *> tabs;
+
+  /* The start index of each tab in the tab bar */
+  std::vector<int> tab_start;
+
+  /* How far the tab bar is offset in small screens */
+  unsigned int tab_draw_offset;
+
   unsigned int selected_tab = 0;
   void drawTabs();
 
  public:
-  TabSet(Yate &yate, Pane *parent, int x, int y, int width, int height, std::vector<std::string> &paths);
+  TabSet(Yate &yate, Pane *parent, int x, int y, int width, int height,
+         std::vector<std::string> &paths);
   TabSet(Yate &yate, Pane *parent, std::istream &saved_state);
   ~TabSet();
-  void addTab(PaneSet* paneset) {
-    tabs.emplace_back(paneset);
-  }
+  void addTab(PaneSet *paneset) { tabs.emplace_back(paneset); }
   void draw() override;
   const std::string &getTitle() override;
   Focusable *getCurrentFocus() override;
